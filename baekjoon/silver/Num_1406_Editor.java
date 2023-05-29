a5b1bc1da360a90939b963e54e94b94f3f6b9e2f
Jayyyyp <pjae9726@gmail.com>
2023-05-29 22:06:47 +0900

    commit

--- baekjoon/silver/Num_1406_Editor.java
+++ baekjoon/silver/Num_1406_Editor.java
@@ -1,5 +1,68 @@
 package baekjoon.silver;
 
-public class Num_1406_Editor {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
 
+public class Num_1406_Editor{
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+        int M = Integer.parseInt(br.readLine());
+
+        Stack<String> leftSt = new Stack<String>();
+        Stack<String> rightSt = new Stack<String>();
+
+        String[] arr = str.split(""); // split() 메소드 : 문자 단위로 분리
+        for(String s : arr) {
+            leftSt.push(s);
+        }
+
+        for(int i = 0; i < M; i++) {
+            String command = br.readLine();
+            char c = command.charAt(0);
+
+            switch(c) {
+                case 'L':
+                    if(!leftSt.isEmpty())
+                        rightSt.push(leftSt.pop());
+
+                    break;
+                case 'D':
+                    if(!rightSt.isEmpty())
+                        leftSt.push(rightSt.pop());
+
+                    break;
+                case 'B':
+                    if(!leftSt.isEmpty()) {
+                        leftSt.pop();
+                    }
+                    break;
+                case 'P':
+                    char t = command.charAt(2);
+                    leftSt.push(String.valueOf(t));
+                    //leftSt.push(st.nextToken());
+
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        // 왼쪽 스택에 있는 데이터들을 모두 오른쪽 스택으로 옮겨 모든 내용을 출력
+        while(!leftSt.isEmpty())
+            rightSt.push(leftSt.pop());
+
+        while(!rightSt.isEmpty())
+            bw.write(rightSt.pop());
+
+        bw.flush();
+        bw.close();
+    }
 }
