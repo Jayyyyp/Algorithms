9ab646d39d4f60efabb9d5b1728cdec02f62d71f
Jayyyyp <pjae9726@gmail.com>
2023-06-06 00:25:12 +0900

    Algorithms

---
+++ recoding/N_M_re.java
@@ -0,0 +1,16 @@
+package recoding;
+
+import java.io.*;
+
+public class N_M_re {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
