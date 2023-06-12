4d1d7f1b39b30cfa4320e44e4d1bd1693139cdf8
Jayyyyp <pjae9726@gmail.com>
2023-06-12 15:53:22 +0900

    Algorithms

--- baekjoon/silver/SearchAlgorithms/Num_7576_Tomato.java
+++ baekjoon/silver/SearchAlgorithms/Num_7576_Tomato.java
@@ -5,6 +5,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Num_7576_Tomato {
+    static int M, N;
+    static int[][] tomatoBox; // 토마토박스 배열
+    static boolean[][] checked; // 체크 배열
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
