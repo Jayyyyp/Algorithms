e53bbf5d981d66c298174f1236da81b2dcce09e3
Jayyyyp <pjae9726@gmail.com>
2023-06-24 10:55:16 +0900

    Algorithms

---
+++ baekjoon/bronze/Num_2522_Star12.java
@@ -0,0 +1,22 @@
+package baekjoon.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Num_2522_Star12 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine()); // 3
+        br.close();
+
+
+
+        System.out.println();
+
+
+    }
+
+}
