1e0bf89d2e9e0f4816f83b5b39c30f4d026d6811
Jayyyyp <pjae9726@gmail.com>
2023-06-29 10:22:37 +0900

    Algorithm

---
+++ baekjoon/bronze/Num2501.java
@@ -0,0 +1,26 @@
+package baekjoon.bronze;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Num2501 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 1; i <= a; i++) {
+            if (a%i == 0){
+                list.add(i);
+            }
+        }
+
+        if (list.size() <= b-1) {
+            System.out.println(0);
+        }else {
+            System.out.println(list.get(b-1));
+        }
+    }
+}
--- recoding/OX_re.java
+++ recoding/OX_re.java
@@ -1,31 +1,32 @@
-package baekjoon.bronze;
+package recoding;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Num_8958_OX {
+public class OX_re {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine()); // TestCase
-        String arr[] = new String[T]; // OX score arr
+        int T = Integer.parseInt(br.readLine());
+        String arr[] = new String[T];
 
-        for(int i = 0; i < T; i++){ // input OX score in arr
+        for(int i = 0; i < T; i++){
             arr[i] = br.readLine();
         }
-        br.close();
-
         for (String s : arr) {
             int count = 0;
             int sum = 0;
-
             for (int j = 0; j < s.length(); j++) {
                 if (s.charAt(j) == 'O') {
                     count++;
-                } else count = 0;
+                } else {
+                    count = 0;
+                }
                 sum += count;
             }
             System.out.println(sum);
         }
+
+        br.close();
     }
 }
