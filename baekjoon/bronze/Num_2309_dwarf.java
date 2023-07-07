0968696ca85ec761e4932c9335f89cb4ab2d88b5
Jayyyyp <pjae9726@gmail.com>
2023-07-07 18:06:52 +0900

    Algorithms\

--- baekjoon/bronze/Num_2309_dwarf.java
+++ baekjoon/bronze/Num_2309_dwarf.java
@@ -1,14 +1,12 @@
 package baekjoon.bronze;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-
 public class Num_2309_dwarf {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int arr[] = new int[9]; // fake dwarfs
+        int arr[] = new int[9]; // fake dwarf
         int sum = 0;
 
         for(int i = 0; i < 9; i++){
